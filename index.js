fcc63f682963a25141a4a0fd5434e850d2953cd6
HEALER <65026164+healer-op@users.noreply.github.com>
2022-03-12 18:19:54 +0530

    🐳

---
+++ index.js
@@ -0,0 +1,38 @@
+// Dependecies
+const colors = require('colors')
+const prompt = require('prompt-sync')()
+const got = require('got')
+const fs = require('fs')
+
+const gitChecker = require('./Controller/gitchecker')
+const wpChecker = require('./Controller/wpchecker')
+
+
+const Main = () => {
+    console.clear()
+    const ascii = fs.readFileSync('./View/ascii.txt', 'utf-8')
+    console.log(ascii.toString().rainbow);
+    websiteConnection()
+}
+
+
+const websiteConnection = async () => {
+    console.log('[#] - Website URL'.gray)
+    const websiteURL = prompt('[#] - Type the website URL ~> (https://example.com) '.cyan);
+
+    try {
+        await got(websiteURL)
+        console.log('\n[!] - Connection estabilished. Diving Deep!\n'.rainbow);
+        checkersManager(websiteURL)
+    } catch (error) {
+        console.log('\n[*] - Connection failure, please verify your URL.'.red);
+    }
+}
+
+const checkersManager = async (websiteURL) => {
+    await gitChecker(websiteURL)
+    await wpChecker(websiteURL)
+}
+
+
+Main()
